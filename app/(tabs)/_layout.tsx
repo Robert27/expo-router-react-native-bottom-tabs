b6fc2385fe6d9595c22bdd9b2ede60d6b2e66c5e
Robert Eggl <github@roberteggl.dev>
2024-11-27 15:47:18 +0100

    demo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,33 @@
 import { Tabs } from "@/components/bottom-tabs";
+import { Platform } from "react-native";
 
 export default function TabLayout() {
+  const isAndroid = Platform.OS === "android";
   return (
-    <Tabs>
+    <Tabs
+    tabBarInactiveTintColor="#C57B57"
+    tabBarActiveTintColor="#F7DBA7"
+    barTintColor="#1E2D2F"
+    rippleColor="#F7DBA7"
+    activeIndicatorColor="#041F1E"
+    sidebarAdaptable
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: () => ({ sfSymbol: "house" }),
+          tabBarIcon: () => (
+            isAndroid ? require('../../assets/icons/home.svg') : { sfSymbol: "house" }
+           ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: "Explore",
-          tabBarIcon: () => ({ sfSymbol: "person" }),
+          tabBarIcon: () => (
+            isAndroid ? require('../../assets/icons/map.svg') : { sfSymbol: "magnifyingglass" }
+           ),
         }}
       />
     </Tabs>
